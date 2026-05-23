e27c082624a735c68f593ae11a5f77893c16c247
AbhiBisure <abhijeetbisure261@gmail.com>
2026-05-23 21:14:51 +0530

    encapsulation

--- login_page_webelements.py
+++ login_page_webelements.py
@@ -1,7 +1,14 @@
-import unittest
-
+import time
 
 class LoginPage():
 
     def __init__(self,page):
-        self.page = page
+        self.email_input = page.get_by_placeholder("email@example.com")
+        self.password_input = page.get_by_placeholder("enter your passsword")
+        self.login_button = page.get_by_role("button", name="Login")
+
+    def login(self, email, password):
+        self.email_input.fill(email)
+        self.password_input.fill(password)
+        self.login_button.click()
+        time.sleep(4)
--- test_login.py
+++ test_login.py
@@ -1,8 +1,5 @@
 import re
 import time
-
-from playwright.sync_api import Playwright, sync_playwright, expect
-
 from login_page_webelements import LoginPage
 
 
@@ -10,11 +7,7 @@ def test_login(setup):
     page=setup
 
     loginpage= LoginPage(page)
-    page.get_by_placeholder("email@example.com").fill("anshika@gmail.com")
-    page.get_by_placeholder("enter your passsword").fill("Iamking@000")
-    page.get_by_role("button",name="Login").click()
-    time.sleep(4)
-
+    loginpage.login("anshika@gmail.com","Iamking@000")
 
 def test_two():
     print("test_two is executed")
